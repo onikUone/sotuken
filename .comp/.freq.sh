aeeb96a4f962b276821abd64f79316e606819eaf
omo <Uone.1997@outlook.jp>
2017-05-25 22:42:44 +0900

    Mac用とさくらVPS用の作成

--- .comp/.freq.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-make3gram < $1 | iconv -f CP932 -t UTF-8 | sort | uniq -c | sed -e 's/^ *//' | sort -r > $2
--- .comp/.makew1gram.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-iconv -f UTF-8 -t CP932 $1 | extraction | iconv -f CP932 -t UTF-8 | mecab -O wakati | sed 's/ /\n/g'
